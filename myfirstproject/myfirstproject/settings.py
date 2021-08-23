b2a7063f5ae93000f1472725b92590f40c7aac87
AndreyPanovA <a.andrey.panov@gmail.com>
2021-08-23 15:16:23 +0300

    superuser|

--- myfirstproject/myfirstproject/settings.py
+++ myfirstproject/myfirstproject/settings.py
@@ -78,8 +78,14 @@ WSGI_APPLICATION = 'myfirstproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pythondev',
+        'USER': 'postgres',
+        'PASSWORD': 'qwe123qwe1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
